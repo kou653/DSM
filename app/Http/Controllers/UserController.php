876bd9f126cc80c09e2d1e8adf46f16dc35ed27a
Kouassi Roxane <rkouassi372@gmail.com>
2026-05-06 12:08:40 +0000

    admin tous projets

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
             'password' => 'required|min:8',
             'role' => 'required|in:administrateur,agent terrain,commanditaire',
             'code_acces' => 'nullable|string',
-            'projects' => 'required|array|min:1',
+            'projects' => 'required_unless:role,administrateur|array',
             'projects.*' => 'exists:projets,id',
         ]);
 
@@ -55,7 +55,7 @@ class UserController extends Controller
             'email' => 'sometimes|email|unique:users,email,' . $user->id,
             'code_acces' => 'nullable|string',
             'role' => 'sometimes|in:administrateur,agent terrain,commanditaire',
-            'projects' => 'required|array|min:1',
+            'projects' => 'required_unless:role,administrateur|array',
             'projects.*' => 'exists:projets,id',
         ]);
 
